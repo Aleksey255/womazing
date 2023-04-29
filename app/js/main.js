c099f7e20f11002d5e1bfc32f47593988a1f5075
Aleksey255 <aleksey-255@mail.ru>
2023-04-29 15:56:31 +0500

    shop

--- app/js/main.js
+++ app/js/main.js
@@ -1,43 +1,47 @@
-$(function(){
-    
-    let header = $('.header');
-  
- $(window).scroll(function() {
-   if($(this).scrollTop() > 1) {
-    header.addClass('header_fixed');
-    $('body').css({
-        'paddingTop': 125+'px' 
-     });
-   } else {
-    header.removeClass('header_fixed');
-    $('body').css({
-        'paddingTop': 0 
-       })
-   }
- });
+$(function () {
 
- $('.top__slider-left').slick({
-  dots: true,
-  arrows: false,
-  fade: true,
-  autoplay: true,
-  asNavFor: '.top__slider-right'
+  let header = $('.header');
+
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 1) {
+      header.addClass('header_fixed');
+      $('body').css({
+        'paddingTop': 125 + 'px'
+      });
+    } else {
+      header.removeClass('header_fixed');
+      $('body').css({
+        'paddingTop': 0
+      })
+    }
+  });
+
+  $('.top__slider-left').slick({
+    dots: true,
+    arrows: false,
+    fade: true,
+    autoplay: true,
+    asNavFor: '.top__slider-right'
+  });
+
+  $('.top__slider-right').slick({
+    dots: false,
+    arrows: false,
+    fade: true,
+    asNavFor: '.top__slider-left'
+  });
+
+  $('.team__slider').slick({
+    dots: true,
+    arrows: true,
+    fade: true,
+  });
+
+   let mixer = mixitup('.shop__inner', {
+    load: {
+        filter: '.all'
+    }
 });
-
-$('.top__slider-right').slick({
-  dots: false,
-  arrows: false,
-  fade: true,
-  asNavFor: '.top__slider-left'
-});
-
-$('.team__slider').slick({
-  dots: true,
-  arrows: true,
-  fade: true,
-});
-
-Fancybox.bind("[data-fancybox]");
 
 
 });
